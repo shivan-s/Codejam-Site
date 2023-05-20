c6a8af30eb0caf74bc08b40f5b84bf5dc43e5950
Shivan Sivakumaran <51132467+ShivanS93@users.noreply.github.com>
2023-05-20 13:27:29 +1200

    feat: adding testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,56 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { type PlaywrightTestConfig, devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	testDir: './tests',
+	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: 'html',
+	use: {
+		baseURL: 'http://localhost:4173',
+		trace: 'on-first-retry'
+	},
+	expect: { timeout: 30000 },
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
+		}
+		/* Test against mobile viewports. */
+		// {
+		//   name: 'Mobile Chrome',
+		//   use: { ...devices['Pixel 5'] },
+		// },
+		// {
+		//   name: 'Mobile Safari',
+		//   use: { ...devices['iPhone 12'] },
+		// },
+
+		/* Test against branded browsers. */
+		// {
+		//   name: 'Microsoft Edge',
+		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+		// },
+		// {
+		//   name: 'Google Chrome',
+		//   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
+		// },
+	],
 	webServer: {
 		command: 'npm run build && npm run preview',
-		port: 4173
-	},
-	testDir: 'tests',
-	testMatch: /(.+\.)?(test|spec)\.[jt]s/
+		url: 'http://localhost:4173',
+		reuseExistingServer: !process.env.CI
+	}
 };
 
 export default config;
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,12 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('index page loads', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('link', { name: "TimeEnjoyed's" })).toBeVisible();
+});
+
+test('navigation works', async ({ page }) => {
+	await page.goto('/');
+	await page.getByRole('link', { name: 'Goal' }).click();
+	await expect(page.getByRole('heading', { name: 'Goal' })).toBeVisible();
 });
